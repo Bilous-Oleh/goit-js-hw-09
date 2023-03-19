fac662202b63e3e4e5b8f6c9fe143cf5a7f1f303
Bilous-Oleh <2005net2005@gmail.com>
2023-03-20 00:35:16 +0200

    Done task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', handleCreatePromise);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+      reject({ position, delay });
+    }, delay);
+  });
+}
+
+function handleCreatePromise(event) {
+  event.preventDefault();
+  const dataForm = new FormData(event.currentTarget);
+  const formElements = {};
+  for (const [key, value] of dataForm.entries()) {
+    formElements[key] = Number(value);
+  }
+
+  let { delay, step, amount } = formElements;
+
+  for (let i = 1; i <= amount; i += 1) {
+    delay += step;
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    formEl.reset();
   }
 }
