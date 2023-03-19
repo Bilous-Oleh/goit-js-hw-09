c761bfd484fc08eddbe6099a61da0da8da949ed8
Bilous-Oleh <114952390+Bilous-Oleh@users.noreply.github.com>
2023-03-19 08:25:35 +0000

    Deploying to gh-pages from @ Bilous-Oleh/goit-js-hw-09@31b20d1cfd6de791650d7f1587ce20dc806b740e 🚀

--- 01-color-switcher.a1db7195.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=null;t.addEventListener("click",(function(){r=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}))}();
-//# sourceMappingURL=01-color-switcher.a1db7195.js.map
